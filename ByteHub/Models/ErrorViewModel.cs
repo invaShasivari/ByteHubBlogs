fb8e4150aadbabe45584c69c2ddc4201b68100cb
invaSh <inva_sh@hotmail.com>
2023-12-12 09:23:17 +0100

    Add project files.

---
+++ ByteHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ByteHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
